83d8cb3b976cc362e421e220e497cb108b856923
yohan <yohan.penaloza@gmail.com>
2022-10-22 22:44:49 -0500

    Fix: validar data recibida en el request para obtener solo los valores del modelo

--- backend-server/src/models/task.models.js
+++ backend-server/src/models/task.models.js
@@ -10,14 +10,6 @@ const taskSchema = new Schema({
   estado: { type: Boolean, default: false },
 });
 
- /* sobrescribir  mombre de la clave _id que mongoose le coloca por default a los registros 
- por uno personalizado*/
-
-/*  taskSchema.method('toJson', function (){
-    const { __v, _id, ...objects} = this.toObject();
-    object.uid = _id;
-    return object
-  }) */
 
 // Convertir a un modelo 
 module.exports = mongoose.model('Task', taskSchema);
